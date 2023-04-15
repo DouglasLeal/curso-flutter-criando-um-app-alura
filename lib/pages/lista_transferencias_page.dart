5c361944a8da23f46722b719c1d7487c3d341c09
Douglas Leal <douglas.leal@live.com>
2023-04-15 20:34:52 -0300

    ListaTransferenciasPage concluida

--- lib/pages/lista_transferencias_page.dart
+++ lib/pages/lista_transferencias_page.dart
@@ -1,24 +1,51 @@
-
-
-
 import 'package:flutter/material.dart';
+import '../models/Transferencia.dart';
+
+const _tituloAppBar = "Transferências";
 
 class ListaTransferenciasPage extends StatefulWidget {
-  const ListaTransferenciasPage({Key? key}) : super(key: key);
+  final List<Transferencia> _transferencias = [];
+
+  ListaTransferenciasPage({Key? key}) : super(key: key);
 
   @override
   State<ListaTransferenciasPage> createState() => _ListaTransferenciasPageState();
 }
 
 class _ListaTransferenciasPageState extends State<ListaTransferenciasPage> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Transferências")),
+      appBar: AppBar(title: const Text(_tituloAppBar)),
       floatingActionButton: FloatingActionButton(
         onPressed: () {  },
         child: const Icon(Icons.add),
       ),
+      body: ListView.builder(
+          itemCount: widget._transferencias.length,
+          itemBuilder: (context, indice) {
+            final transferencia = widget._transferencias[indice];
+            return ItemTransferencia(transferencia);
+          },)
     );
   }
 }
+
+class ItemTransferencia extends StatelessWidget {
+  ItemTransferencia(this._transferencia, {Key? key}) : super(key: key);
+
+  final Transferencia _transferencia;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+}
+
