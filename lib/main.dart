6d33f622dfb91298cf1400d29505a17382dbb303
Douglas Leal <douglas.leal@live.com>
2023-04-15 17:25:53 -0300

    Criando appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,13 @@
-
 import 'package:flutter/material.dart';
 
-void main() => runApp(Text("teste", textDirection: TextDirection.ltr,));
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(title: const Text("Transferências")),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {  },
+            child: const Icon(Icons.add),
+          ),
+        ),
+      ),
+    );
