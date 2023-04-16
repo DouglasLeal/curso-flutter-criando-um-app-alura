1f59133ad98f73eabe9071915cccfe3c9c8b37a9
Douglas Leal <douglas.leal@live.com>
2023-04-15 21:25:32 -0300

    Alterando tema

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,20 @@ import 'package:flutter/material.dart';
 
 import 'pages/lista_transferencias_page.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: ListaTransferenciasPage()
+void main() => runApp(BytebankApp());
+
+class BytebankApp extends StatelessWidget {
+  const BytebankApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+            primary: Colors.green[900],
+            secondary: Colors.blueAccent[700]),
       ),
+      home: ListaTransferenciasPage(),
     );
+  }
+}
